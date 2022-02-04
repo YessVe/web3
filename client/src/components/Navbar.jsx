ce594baddcae84c583a816bb6d93c006987ebcf6
Yessenia <vela.kathy@gmail.com>
2022-02-03 21:53:44 -0500

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+import React, { useState } from 'react';
+
+const NavbarItem = ({ tittle, classProps }) => {
+    return(
+        <li className={`mx-4 cursor pointer ${classProps}`}>
+            {tittle}
+        </li>
+    )
+}
+
+const Navbar = ()=>{
+    // con el state se indicará su propiedad responsive de la web
+    const [toggleMenu, setToggleMenu] = useState(false);
+    
+    return(
+        <nav className='w-full flex md:justify-center'>
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <img src={logo} alt='logo' className='w-32 cursor-pointer'/>
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex initial'>
+                {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index)=>(
+                // item será market, exchange, etc. y eso dará vueltas
+                // for each interation I want to show:    
+                    <NavbarItem key={item+index} tittle={item} />
+                // el key es parte del dom virtual
+                ))}
+
+                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                    Login
+                </li>
+            </ul>
+            <div className='flex relative'> 
+                {toggleMenu
+                    ? <AiOutlineClose fontSize={28} className='text-white md: hidden cursor-pointer' onClick={()=> setToggleMenu(false)} />
+                    : <HiMenuAlt4  fontSize={28} className='text-white md: hidden cursor-pointer' onClick={()=> setToggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide'>
+                        <li className='text-xl w-full m-2'>
+                            <AiOutlineClose onClick={()=> setToggleMenu(false)} />
+                        </li>
+                        {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index)=>(
+                        // item será market, exchange, etc. y eso dará vueltas
+                        // for each interation I want to show:    
+                            <NavbarItem key={item+index} tittle={item} classProps='my-2 text-lg'/>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
